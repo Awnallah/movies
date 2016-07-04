509517d1d38a22bae67ec2c062745b89c75891dd
Hamzah <6hamzah6@gmail.com>
2016-07-04 12:35:18 -0700

    extract movie data form cvs file

--- entertainment.py
+++ entertainment.py
@@ -1,22 +1,34 @@
 import fresh_tomatoes
 import media
+import csv
+
+# toy_story = media.Movie("Toy Story",
+#  						'A story of a boy and his toys that came to life',
+#  						'http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
+#  						'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+
+# #print(toy_story.storyline)
+
+# avatar = media.Movie('Avatar',
+# 					'A marine on an alien planet',
+# 					'https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg',
+# 					'https://www.youtube.com/watch?v=5PSNL1qE6VY')
+# #avatar.show_trailer()
+# #print(avatar.storyline)
+# #toy_story.show_trailer()
+
+def pull_movies(file):
+    """ pulls movie data from excel and returns a list of Movie objects """
+    movies = []
+    with open(file, 'rb') as csvfile:
+        movie_table = csv.DictReader(csvfile)
+        for movie in movie_table:
+            movies.append(media.Movie(movie_title=movie['movie_title'],
+                                poster_image=movie['poster_image'],
+                                trailer_youtube=movie['trailer_youtube']))
+    return movies
 
 
-toy_story = media.Movie("Toy Story",
- 						'A story of a boy and his toys that came to life',
- 						'http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
- 						'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 
-#print(toy_story.storyline)
-
-avatar = media.Movie('Avatar',
-					'A marine on an alien planet',
-					'https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg',
-					'https://www.youtube.com/watch?v=5PSNL1qE6VY')
-#avatar.show_trailer()
-#print(avatar.storyline)
-#toy_story.show_trailer()
-
-movies = [toy_story, avatar]
+movies = pull_movies('movieData/movieData.csv')
 fresh_tomatoes.open_movies_page(movies)
-print(media.Movie.__doc__)
